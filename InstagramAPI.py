4439efa162b5911f62c58d908e525e42fbcb2a50
Mattia-Rigotti <mattia.rigotti@live.it>
2014-04-14 13:58:11 +0200

    image data fetch
    now also prints the urls of the images and returns a dictionary fetched
    from the json in the link previously returned

--- InstagramAPI.py
+++ InstagramAPI.py
@@ -1,22 +1,24 @@
 from lib.instagram_lbs.instagram import client, subscriptions
 from lib.instagram_lbs.instagram.client import InstagramAPI
+import lib.instagram_lbs.instagram.simplejson as json
 import urllib2
 
 class Instagram_API(object):
 	
 	def __init__(self,client_id,client_secret):
-                self.client_id=client_id
-                self.client_secret=client_secret
+		self.client_id=client_id
+		self.client_secret=client_secret
 		self.api = InstagramAPI(self.client_id, self.client_secret)
 
 ## Gets the user ID from the given username
-## I suppose it would be better to use some kind of json library
 	def getUserID(self,username):
 		url='https://api.instagram.com/v1/users/search?q='+username+'&client_id='+self.client_id
 		response = urllib2.urlopen(url)
 		html = response.read()
-		html=html.split('"')
-		return html[len(html)-2] 
+		dct=json.loads(html)
+		return dct['data'][0]['id']
+		#html=html.split('"')
+		#return html[len(html)-2] 
 
 ## Method to get the photos from a keyword	
 	def getPhotos(self, keyword, amount, last_photoid, new=True):
@@ -28,14 +30,28 @@ class Instagram_API(object):
 				user_media = self.api.user_recent_media(user_id=uid, count=amount, min_id=last_photoid)
 			else:
 				user_media = self.api.user_recent_media(user_id=uid, count=amount, max_id=last_photoid)
-			return user_media
+			url=user_media[1]
+			response = urllib2.urlopen(url)
+			html = response.read()
+			dct=json.loads(html)
+			for image in dct['data']:
+                                url= image['images']['standard_resolution']['url']
+                                print url
+			return dct
 		elif keyword[0]=="#":
 			keyword=keyword.split('#',1)
 			if new == True:
 				hash_media = self.api.tag_recent_media(count=amount, min_id=last_photoid, tag_name=keyword[1])
 			else:
 				hash_media = self.api.tag_recent_media(count=amount, max_id=last_photoid, tag_name=keyword[1])
-			return hash_media
+			url=hash_media[1]
+			response = urllib2.urlopen(url)
+			html = response.read()
+			dct=json.loads(html)
+			for image in dct['data']:
+                                url= image['images']['standard_resolution']['url']
+                                print url
+			return dct
 				
 		else:
 			print "Error, you have to provide either an @username or an #hashtag"
